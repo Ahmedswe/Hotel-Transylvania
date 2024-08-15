bd277e8a76a84ad1a1cb6b97c3cf22a8afecb944
Ahmedswe <abbasbutt2020@outlook.com>
2024-08-16 00:44:04 +0500

    finishing project

--- login_page.java
+++ login_page.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class login_page extends JPanel implements ActionListener {
 
@@ -16,13 +17,14 @@ public class login_page extends JPanel implements ActionListener {
     private JLabel password_label = new JLabel("Enter Password");
     private JTextField ID_field = new JTextField();
     private JPasswordField password_field = new JPasswordField();
-
+    private JLabel error_msg = new JLabel("");
+    private int Id = 231380007;
+    private String password = "OOP";
 
     login_page(int[] size) {
         this.size = size;
         this.setLayout(null);
-        this.setBounds(0, 0, size[0] , size[1]);
-
+        this.setBounds(0, 0, size[0], size[1]);
 
         try {
             bg_img = ImageIO.read(new File("Hotel_Transylvania.png"));
@@ -47,12 +49,17 @@ public class login_page extends JPanel implements ActionListener {
 
         login_btn.setBounds((size[0] / 2) - 50, (int) (size[1] / 2), 100, 30);
 
+        
+        error_msg.setFont(boldFont);
+        error_msg.setForeground(Color.red);
+        error_msg.setBounds(size[0]/2-90, (int) (size[1] / 1.7), 200 ,30);
 
         this.add(ID_label);
         this.add(ID_field);
         this.add(login_btn);
         this.add(password_field);
         this.add(password_label);
+        this.add(error_msg);
         login_btn.addActionListener(this);
     }
 
@@ -67,20 +74,41 @@ public class login_page extends JPanel implements ActionListener {
     }
 
     @Override
+
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == login_btn) {
+            String idInput = ID_field.getText().trim();
+            char[] passwordInput = password_field.getPassword();
 
+            
+            if (idInput.isEmpty() || passwordInput.length == 0) {
+                error_msg.setText("Fill all the required fields");
+                return;
+            }
+
+            
+            String passwordInputString = new String(passwordInput);
+            boolean validCredentials = idInput.equals(String.valueOf(Id)) && passwordInputString.equals(password);
+
+            if (!validCredentials) {
+                error_msg.setText("Invalid credentials");
+                Arrays.fill(passwordInput, ' '); 
+                return;
+            }
+
+            
             JFrame parent = (JFrame) SwingUtilities.getWindowAncestor(this);
             parent.remove(this);
             this.size[0] = 500;
             this.size[1] = 500;
             JPanel hotelPanel = new Hotel_transylvania(this.size);
-            hotelPanel.setBounds(0, 0, size[0] , size[1]);
-            parent.setSize(this.size[0] , this.size[0]);
+            hotelPanel.setBounds(0, 0, size[0], size[1]);
+            parent.setSize(this.size[0], this.size[1]); 
             parent.add(hotelPanel);
 
             parent.revalidate();
             parent.repaint();
         }
     }
+
 }
